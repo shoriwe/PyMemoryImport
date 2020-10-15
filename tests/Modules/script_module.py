1bc659eac60d6f605dec40f3733161a0856cfb54
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-10-14 21:12:32 -0500

    Create script_module.py

---
+++ tests/Modules/script_module.py
@@ -0,0 +1,7 @@
+# adding dependencies to the module script
+import string
+
+
+def hello_script():
+    print("Hello!! from a script module")
+    print("Testing dependencies (string, )", string.ascii_letters)
