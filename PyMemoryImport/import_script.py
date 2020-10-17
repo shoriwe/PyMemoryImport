3ec75aa3d3d7d9d36a2e64e086eca9ca9c12b3a4
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-10-16 21:13:14 -0500

    Update import_script.py
    Now the module_name is optional

--- PyMemoryImport/import_script.py
+++ PyMemoryImport/import_script.py
@@ -3,7 +3,9 @@ import types
 __all__ = ["import_script_from_bytes"]
 
 
-def import_script_from_bytes(module_name: str, script_bytes: bytes) -> types.ModuleType:
+def import_script_from_bytes(script_bytes: bytes, module_name: str = None) -> types.ModuleType:
+    if module_name is None:
+        module_name = "<Module>"
     script_module = types.ModuleType(module_name)
     exec(script_bytes, script_module.__dict__, script_module.__dict__)
     return script_module
